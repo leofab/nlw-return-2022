43032fedf4f236e131c9881875e17d321fc6fdf4
Léo Fabrício <leofabr3@gmail.com>
2022-05-02 12:50:35 -0300

    components and props review

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+interface ButtonProps {
+  text: string;
+}
+
+function Button(props: ButtonProps) {
+  return <button>{props.text}</button>;
+}
 
 function App() {
-  return <h1>Hello, world!</h1>;
+  return <div>
+    <Button text="Hello" />
+    <Button text="ok" />
+  </div>;
 }
 
 export default App
