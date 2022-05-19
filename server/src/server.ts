0cf815d5f539dc1d6344c5d3fd5e8bac993b589f
Léo Fabrício <leofabr3@gmail.com>
2022-05-19 11:09:45 -0300

    prisma config 2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,27 @@
+import { prisma } from '@prisma/client';
 import express from 'express'
 
 const app = express();
 
-app.get('/users', (req,res) => {
+// GET, POST, PUT, PATCH, DELETE
+
+//GET = Buscar informações
+//POST = Criar informações
+//PUT = Atualizar informações
+//PATCH = Atualizar informações parcialmente
+//DELETE = Deletar informações
+
+app.use(express.json());
+
+app.post('/feedbacks', (req,res) => {
+    prisma.feedback.create({
+        data: {
+            type: req.body.type,
+            comment: req.body.comment,
+            screenshot: req.body.screenshot,
+        }
+    })
+    
     return res.send("Hello World");
 })
 
