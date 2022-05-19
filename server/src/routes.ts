c2c5a444d43e9215e70c88365b4e76dc4969deb6
leofab <leofabr3@gmail.com>
2022-05-19 18:18:22 -0300

    jest implement

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,4 @@
-import { prisma } from './prisma';
 import express from 'express';
-import nodemailer from 'nodemailer';
 import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedbacks-repository';
 import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-adapter';
