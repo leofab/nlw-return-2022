31a1bdfcdda27c833567277de1803417042bc04e
leofab <leofabr3@gmail.com>
2022-05-20 12:57:40 -0300

    install apploading fonts & phosphor+svg

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { View } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+
 
 import { styles } from './styles';
 
 export function Widget() {
   return (
-    <View style={styles.container}>
-
-    </View>
+    <>
+    <TouchableOpacity>
+        
+    </TouchableOpacity>
+    </>
   );
 }
